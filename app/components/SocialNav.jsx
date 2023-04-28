9ca01bbd109d690d039b725a079eb60ff99dbd57
Bruno M <brunomoron56@gmail.com>
2023-04-27 23:27:52 -0300

    first version of socialNav

--- app/components/SocialNav.jsx
+++ app/components/SocialNav.jsx
@@ -1,11 +1,20 @@
+"use client";
 import Image from "next/image";
 import linkedinIco from "../../public/linkedin.svg";
 import gitIco from "../../public/git.svg";
 import mailIco from "../../public/mail.svg";
+import openIco from "../../public/upArrow.svg";
+import closeIco from "../../public/dowArrow.svg";
+import { useState } from "react";
 
-function socialNav() {
+function SocialNav() {
+  const [open, setOpen] = useState(false);
+  
+  function handleClick() {
+    setOpen(!open);
+  }
   return (
-    <nav>
+    <nav className="bg-slate-500 inline-block">
       <ul>
         <li>
           <a href="http://" target="_blank" rel="noopener noreferrer">
@@ -19,12 +28,29 @@ function socialNav() {
         </li>
         <li>
           <a href="http://" target="_blank" rel="noopener noreferrer">
-            <Image alt="" src={mailIco} width={35} height={35}></Image>    
-        </a>
+            <Image alt="" src={mailIco} width={35} height={35}></Image>
+          </a>
         </li>
       </ul>
+      {open ? (
+        <Image
+          onClick={handleClick}
+          alt=""
+          src={closeIco}
+          width={30}
+          height={30}
+        ></Image>
+      ) : (
+        <Image
+          onClick={handleClick}
+          alt=""
+          src={openIco}
+          width={30}
+          height={30}
+        ></Image>
+      )}
     </nav>
   );
 }
 
-export default socialNav;
+export default SocialNav;
