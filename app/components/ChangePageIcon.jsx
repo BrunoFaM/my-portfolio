3bb19ccedcbe30463bf3b7d6c9309f2d9faa1395
Bruno M <brunomoron56@gmail.com>
2023-06-07 13:11:40 -0300

    upgrading the home page

---
+++ app/components/ChangePageIcon.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import leftArrow from "../../public/leftArrow.svg";
+import rightArrow from "../../public/rightArrow.svg";
+import Link from "next/link";
+import Image from "next/image";
+
+function ChangePageIcon({ direction, url, hacia }) {
+  let d;
+  let icon;
+  if(direction == "l"){
+    d = "left";
+    icon = leftArrow;
+  }else{
+    d = "right"
+    icon = rightArrow; 
+  }
+  return (
+    <div className={`absolute top-1/2 m-2 right-0 `}>
+      <Link href={url} title={hacia}>
+        <Image
+          alt="icono"
+          width={40}
+          height={40}
+          src={icon}
+        ></Image>
+      </Link>
+    </div>
+  );
+}
+
+export default ChangePageIcon;
--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
+import ChangePageIcon from "./components/ChangePageIcon";
+
 export default function Home() {
   return (
-    <main className="h-screen w-screen flex items-center gap-5">
-      <h1 className="text-3xl">
-        <hr className=" border-none"/>
-        <span>soy Bruno Moron,</span>
+    <main className="h-full w-screen  mt-20 ">
+      <h1 className="text-6xl p-6 text-gray-300">
         <hr className=" border-none" />
-        <span>desarrollador web</span>
+        <span>PROGRAMADOR</span>
+        <hr className=" border-none" />
+        <span className="float-right">WEB</span>
       </h1>
+      <ChangePageIcon  direction={"right"} url={"/about"} hacia={"sobre mi"} />
+      <p className="absolute bottom-3 pl-1">@2023</p>
     </main>
   );
 }
