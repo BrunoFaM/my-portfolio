e3f25c4dc876db76513eba8c06c916544b7adf94
Bruno M <brunomoron56@gmail.com>
2023-08-08 17:18:42 -0300

    adding backend icons

--- app/pages/AboutSection.jsx
+++ app/pages/AboutSection.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import javaIco from "../../public/java.svg"
+import springIco from "../../public/spring.svg"
+import mySqlIco from "../../public/mySql.svg"
+import springBootIco from "../../public/SBoot.svg"
 
 function AboutSection() {
   return (
-    <section className="bg-base  w-screen font-libre p-3">
-      <section className="resumen">
-        <h1 className="text-3xl font-libreBold">Sobre Mi</h1>
-        <p>Soy estudiante de tecnicatura en programacion y ademas un estudiante autodidacta y apasionado del desarrollo tanto <span className="font-libreBold">Front-end</span> como <span className="font-libreBold">Back-end</span>.</p>
-      </section>
-      <section className="tecnologias">
-        <h1 className="text-3xl font-libreBold">Tecnologias</h1>
-      </section>
+    <section className="bg-base min-h-screen  w-screen font-libre p-3">
+      <div className="resumen">
+        <h1 className="text-3xl text-center mb-5 font-libreBold">Sobre Mi</h1>
+        <p className="text-left p-1 leading-relaxed text-xl">
+          Soy estudiante de tecnicatura en programacion y ademas un estudiante
+          autodidacta y apasionado del desarrollo tanto{" "}
+          <span className="font-libreBold">Front-end</span> como{" "}
+          <span className="font-libreBold">Back-end</span>.
+        </p>
+      </div>
+      <div className="tecnologias mt-5 ">
+        <h1 className="text-3xl font-libreBold text-center">Tecnologias</h1>
+        <div className="flex">
+          <Image alt='' src={javaIco} height={100} width={100} ></Image>
+          <Image alt='' src={springIco} height={100} width={100} ></Image>
+          <Image alt='' src={springBootIco} height={100} width={100}  ></Image>
+          <Image alt='' src={mySqlIco} height={100} width={100}  ></Image>
+
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default AboutSection
+export default AboutSection;
--- app/pages/MainSection.jsx
+++ app/pages/MainSection.jsx
@@ -3,7 +3,7 @@ import ChangePageIcon from "../components/ChangePageIcon";
 
 function MainSection() {
   return (
-    <section className="h-screen w-screen  mt-20 lg:mt-32 ">
+    <section className="h-[calc(100vh-100px)] w-screen  mt-20 lg:mt-32 ">
       <div className="mt-0 mb-0 ml-0 min-w-fit max-w-[430px] sm:max-w-[580px] md:max-w-[700px] md:m-auto lg:max-w-[1200px] ">
         <h1 className="text-5xl sm:text-6xl md:text-7xl md:p-3 lg:text-9xl  p-2 text-gray-300  flex flex-col">
           <hr className=" border-none" />
